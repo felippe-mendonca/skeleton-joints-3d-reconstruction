7c7282e16a435ff0f7e8829dcaf1ab9b16d1511f
Felippe <mendonca.felippe@gmail.com>
2019-03-27 13:57:44 -0300

    added experiment files of skeleton localization using detections

--- etc/experiments/request_skeleton_localization/run.sh
+++ etc/experiments/request_skeleton_localization/run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-./setup.sh exp0 160224
-./setup.sh exp0 160226
-./setup.sh exp0 160422
-./setup.sh exp0 161202
+./setup.sh exp3 160224
+./setup.sh exp3 160226
+./setup.sh exp3 160422
+./setup.sh exp3 161202
 
-./setup.sh exp1 160224
-./setup.sh exp1 160226
-./setup.sh exp1 160422
-./setup.sh exp1 161202
+./setup.sh exp4 160224
+./setup.sh exp4 160226
+./setup.sh exp4 160422
+./setup.sh exp4 161202
 
-./setup.sh exp2 160224
-./setup.sh exp2 160226
-./setup.sh exp2 160422
-./setup.sh exp2 161202
+./setup.sh exp5 160224
+./setup.sh exp5 160226
+./setup.sh exp5 160422
+./setup.sh exp5 161202
