6a314409d18a17eda879be4f9f723ca5fc0e6b3a
Felippe <mendonca.felippe@gmail.com>
2019-03-28 19:09:26 -0300

    new experiments of detection and localization added

--- etc/experiments/request_skeleton_localization/run.sh
+++ etc/experiments/request_skeleton_localization/run.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+./setup.sh exp0 160224
+./setup.sh exp0 160226
+./setup.sh exp0 160422
+./setup.sh exp0 161202
+
+./setup.sh exp1 160224
+./setup.sh exp1 160226
+./setup.sh exp1 160422
+./setup.sh exp1 161202
+
+./setup.sh exp2 160224
+./setup.sh exp2 160226
+./setup.sh exp2 160422
+./setup.sh exp2 161202
+
 ./setup.sh exp3 160224
 ./setup.sh exp3 160226
 ./setup.sh exp3 160422
